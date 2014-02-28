d37912b2ead3cc39d7f022a7ff556c603839c1de
He Lin <lin-he-he@163.com>
2014-02-28 12:00:33 +0800

    项目结束
    事实证明。Winsock在Win7环境下受到一定的限制。
    这最终让我放弃了Winsock这一套API。
    Limitations on Raw Sockets
    On Windows 7, Windows Vista, Windows XP with Service Pack 2 (SP2), and
    Windows XP with Service Pack 3 (SP3), the ability to send traffic over
    raw sockets has been restricted in several ways:
    
    TCP data cannot be sent over raw sockets.
    UDP datagrams with an invalid source address cannot be sent over raw
    sockets. The IP source address for any outgoing UDP datagram must exist
    on a network interface or the datagram is dropped. This change was made
    to limit the ability of malicious code to create distributed
    denial-of-service attacks and limits the ability to send spoofed packets
    (TCP/IP packets with a forged source IP address).
    A call to the bind function with a raw socket for the IPPROTO_TCP
    protocol is not allowed.
    Note  The bind function with a raw socket is allowed for other protocols
    (IPPROTO_IP, IPPROTO_UDP, or IPPROTO_SCTP, for example).
    
    These above restrictions do not apply to Windows Server 2008 R2, Windows
    Server 2008 , Windows Server 2003, or to versions of the operating
    system earlier than Windows XP with SP2.

--- HeheSocketConnectionTest/main.cpp
+++ HeheSocketConnectionTest/main.cpp
@@ -4,6 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <process.h>
+struct IPHeader{
+	unsigned char ver_hlen;
+	unsigned char tos;
+	unsigned short total_len;
+	unsigned short ident;
+	unsigned short frag_and_flags;
+	unsigned char ttl;
+	unsigned char proto;
+	unsigned short checksum;
+	unsigned int sourceIP;
+	unsigned int destIP;
+};
+struct ICMPHeader{
+	unsigned char type;
+	unsigned char code;
+	unsigned short cksum;
+	unsigned short id;
+	unsigned short seq;
+	//char  fucker[10];
+};
 int UDP_flood(char * flood_str,unsigned int times,sockaddr_in &DestAddr){
 	int ErrorCode=0;
 	SOCKET sock;
@@ -49,16 +70,161 @@ int TCP_flood(char * flood_str,unsigned int times,sockaddr_in &DestAddr){
 	WSACleanup();
 	printf("TCP_Flood_Finished\n");
 }
+#define ICMP_ECHO_REPLY 0
+#define ICMP_ECHO_REQUEST 8
+#define PACKAGE_SIZE sizeof(IPHeader)+sizeof(ICMPHeader)
+#define xmalloc(s) HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,(s))
+#define xfree(p) HeapFree(GetProcessHeap(),0,(p))
+void usage();
+void HandleError(char *);
+void FillPackage(char *,unsigned long,unsigned long,u_short);
+USHORT CheckSum(USHORT *,int);
+
+int ICMP_ping(int times,sockaddr_in from,sockaddr_in to){
+	IPHeader iphdr;
+	ICMPHeader icmphdr;
+	unsigned long srcIP=from.sin_addr.s_addr;
+	unsigned long dstIP=to.sin_addr.s_addr;
+	bool bBroadcast=false;
+	int pid=_getpid();
+	int len=sizeof(to);
+	WSAData wsaData;
+	WSAStartup(WINSOCK_VERSION,&wsaData);
+	SOCKET sock=socket(AF_INET,SOCK_RAW,IPPROTO_ICMP);
+	if(sock==INVALID_SOCKET){
+		HandleError("socket");
+		WSACleanup();
+		return -1;
+	}
+	BOOL on=TRUE;
+	if(setsockopt(sock,IPPROTO_IP,IP_HDRINCL,(char*)&on,sizeof(on))==SOCKET_ERROR){
+		HandleError("setsockopt");
+		WSACleanup();
+		return -1;
+	}
+	//bind(sock,(sockaddr*)&from,sizeof(from));
+	char * buf=(char*)xmalloc(PACKAGE_SIZE);
+	struct timeval tv;
+	fd_set readSet;
+	BOOL bError=false;
+
+	for(int i=0;i<times && !bError;i++){
+		printf("ping %s ... %d/n",inet_ntoa(to.sin_addr),i+1);
+		FillPackage(buf,srcIP,dstIP,(u_short)pid);
+		if(sendto(sock,buf,PACKAGE_SIZE,0,(struct sockaddr *)&to,len)==SOCKET_ERROR){
+			HandleError("sendto");
+			break;
+		}
+		while(1){
+			tv.tv_sec=3;
+			tv.tv_usec=0;
+			FD_ZERO(&readSet);
+			FD_SET(sock,&readSet);
+			int res=select(sock+1,&readSet,NULL,NULL,&tv);
+			if(res==SOCKET_ERROR){
+				HandleError("select");
+				bError=true;
+				break;
+			}
+			if(res==0){
+				if(!bBroadcast){
+					printf("time out!\n");
+				}
+				break;
+			}
+			if(FD_ISSET(sock,&readSet)){
+				memset(buf,0,PACKAGE_SIZE);
+				memset(&from,0,sizeof(from));
+				len=sizeof(from);
+				if(recvfrom(sock,buf,PACKAGE_SIZE,0,(struct sockaddr*)&from,&len)==SOCKET_ERROR){
+					HandleError("recvfrom");
+					bError = true;
+					break;
+				}
+				IPHeader * pIPHdr=(IPHeader *)buf;
+				ICMPHeader * pICMPHeader=(ICMPHeader *) (buf + sizeof(IPHeader));
+				if(pICMPHeader->id==htons((u_short)pid)
+					&& pICMPHeader->seq==htons((u_short)pid)
+					&& pICMPHeader->type==ICMP_ECHO_REPLY){
+						printf("Echo reply from %s \n",inet_ntoa(from.sin_addr));
+						if(!bBroadcast)break;
+				}
+					
+
+			}
+		}
+	}
+	xfree(buf);
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+void FillPackage(char * pData,unsigned long srcIP,unsigned long dstIP,u_short id){
+	memset(pData,0,PACKAGE_SIZE);
+	IPHeader * pIPHeader=(IPHeader *)pData;
+	int nVersion=4;
+	int nHeadSize=sizeof(IPHeader)/4;
+	unsigned long destIP=dstIP;
+	pIPHeader->ver_hlen=(nVersion<<4)|nHeadSize;
+	pIPHeader->tos=0;
+	pIPHeader->total_len=htons(PACKAGE_SIZE);
+	pIPHeader->ident=htons(1234);
+	pIPHeader->frag_and_flags=0;
+	pIPHeader->ttl=255;
+	pIPHeader->proto=IPPROTO_ICMP;
+	pIPHeader->checksum=0;
+	pIPHeader->sourceIP=srcIP;
+	
+	pIPHeader->destIP=destIP;
+	pIPHeader->checksum=CheckSum((USHORT*)pData,sizeof(IPHeader));
+	ICMPHeader *pICMPHeader = (ICMPHeader*)(pData+sizeof(IPHeader));
+	pICMPHeader->type=ICMP_ECHO_REQUEST;
+	pICMPHeader->code=0;
+	pICMPHeader->cksum=0;
+	pICMPHeader->id=htons(id);
+	pICMPHeader->seq=htons(id);
+	//strcpy(pICMPHeader->fucker,"fuckyoumom");
+	pICMPHeader->cksum=CheckSum((USHORT *)((char *)pData + sizeof(IPHeader)),sizeof(ICMPHeader));
+	
+}
+USHORT CheckSum(USHORT * pUShort,int size){
+	unsigned long cksum=0;
+	while(size>1){
+		cksum+=*pUShort++;
+		size-=sizeof(USHORT);
+	}
+	if(size){
+		cksum+=*(UCHAR *)pUShort;
+
+	}
+	cksum=(cksum>>16)+(cksum&0xffff);
+	cksum+=(cksum>>16);
+	return (USHORT)(~cksum);
+}
+void usage(){
+	printf("usage:ping host IP");
+}
+void HandleError(char * func){
+	int errCode=WSAGetLastError();
+	char info[65]={0};
+	_snprintf(info,64,"%s:%d\n",func,errCode);
+	printf(info);
+}
 int main(){
-	char * FAKE_IP="10.82.38.44";
-	char * SYN_DEST_IP="202.113.18.210";
-	struct sockaddr_in DestAddr;
+	char * FAKE_IP="10.2.124.2";
+	char * SYN_DEST_IP="10.2.124.33";
+	struct sockaddr_in SrcAddr,DestAddr;
+	memset(&SrcAddr,0,sizeof(SrcAddr));
+	SrcAddr.sin_family=AF_INET;
+	SrcAddr.sin_addr.s_addr=inet_addr(FAKE_IP);
+	SrcAddr.sin_port=htons(80);
 	memset(&DestAddr,0,sizeof(DestAddr));
 	DestAddr.sin_family=AF_INET;
 	DestAddr.sin_addr.s_addr=inet_addr(SYN_DEST_IP);
 	DestAddr.sin_port=htons(80);
-
-	UDP_flood("FuckYourMom!!!",100,DestAddr);
-	TCP_flood("FuckYourDad!!!",10,DestAddr);
+	//UDP_flood("FuckYourMom!!!",100,DestAddr);
+	//TCP_flood("FuckYourDad!!!",10,DestAddr);
+	ICMP_ping(3,SrcAddr,DestAddr);
 	system("PAUSE");
 }
